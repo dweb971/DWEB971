d67aece5e24ca62092f33fe3b0065868c5b2e9da
AMBROSIO Martine <rsma.simplon21@gmail.com>
2020-12-14 11:27:11 -0400

    Mon CV

---
+++ cv2020/js/tirage.js
@@ -0,0 +1,5 @@
+/*  tableau des apprenants */
+const apprenants = ["POULIER", "MUSQUET", "HENRI", "LAKE", "ANTOINE", "CICERON", "ELICE", "ELELOUE", "SYDNEY", "DAUPIN", "BIANAY"];
+let index = Math.floor(Math.random() * apprenants.length);
+
+alert(index);
