c5ba7a31882ad7ebcd8b70d8cd611c6b6e4d57fb
dweb971 <73900315+dweb971@users.noreply.github.com>
2021-06-30 15:54:35 -0400

    Add files via upload

---
+++ resthome.gp/traitement.php
@@ -0,0 +1,49 @@
+<?php 
+session_start();
+
+ini_set('display_errors', 1);
+
+// autoloader
+spl_autoload_register(function ($name) {
+    echo "Tentative de chargement de $name.\n";
+    throw new Exception("Impossible de charger $name.");
+});
+
+try {
+   // instanciation des classes
+   include("scripts/Connect.php");
+   include("scripts/Resthome.php");
+
+
+
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";    
+}
+
+$db = new Connect();
+
+$resthome = new Resthome($db);
+
+
+if( isset($_POST["form"]))
+{
+    $nomF = $_POST["form"];
+} else {
+    // connexion a voir
+    $resthome->login($_POST);
+}
+
+switch ($nomF) {
+    case 'addR':
+        # code...
+        $resthome->addR($_POST);
+        break;
+    case 'delR' :
+        # delete recette
+        $resthome->delR($_POST["id"]);
+        break;
+    
+    default:
+        # code...
+        break;
+}
